2614c95f826d24f6e1d77b77f71707a174f8739d
gringgyy <kulnis33@gmail.com>
2021-08-28 14:20:29 +0700

    add tic tac toe project

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Kulnis Chattratitiphan on 28/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
